d79e694b45448e0b91dc0b33378aed6d241a33e4
14Aparajita <143332522+14Aparajita@users.noreply.github.com>
2024-11-15 17:08:56 +0530

    Update Tables_project.sql

--- Tables_project.sql
+++ Tables_project.sql
@@ -30,8 +30,6 @@ CREATE TABLE IF NOT EXISTS `contact_queries` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table url_prediction.contact_queries: ~1 rows (approximately)
-INSERT INTO `contact_queries` (`id`, `name`, `email`, `message`, `submitted_at`) VALUES
-	(1, 'vfvf', 'aparajita@ssipmt.com', 'dfvfbfggbsrgtbrth', '2024-08-08 14:13:53');
 
 -- Dumping structure for table url_prediction.reported_urls
 CREATE TABLE IF NOT EXISTS `reported_urls` (
@@ -63,13 +61,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table url_prediction.users: ~6 rows (approximately)
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `phone`) VALUES
-	(1, 'sjsj', 'pbkdf2:sha256:600000$syArFMbN8ktYYO1V$ca3b0e4e3f26b3a93d17eea7fd362b090afdda18d964aa72ed689d3bf377e91c', 'aashutosh.vaish2023@ssipmt.com', '7712772989'),
-	(2, 'jvbdj', 'pbkdf2:sha256:600000$TNREbgUfzhMhid84$0d05a3018a3976e2e96221ff4bb66efff641c182b47034c2a056fc80a112b2dc', 'aashutosh.vaish2023@ssipmt.com', '7712772989'),
-	(3, 'sjcbsdcij', 'pbkdf2:sha256:600000$3tAAiA4CCFNrjiRE$7c1134bbe64c0abc7571435b5a1a872cb44263d68fb79b4b0acef3e17e2ac3d2', 'aashutosh.vaish2023@ssipmt.com', '7712772989'),
-	(4, 'jvbcjdv', 'pbkdf2:sha256:600000$MgQth7GlXHlKgVly$6636e6e1d8ca2b7a4c92cf0325f7af74e002ba8ce982a68b7adc557e6be74eb2', 'aparajita@ssipmt.com', '987654324'),
-	(5, 'apii', 'pbkdf2:sha256:600000$YPJfwB1p7USZxAJp$b182f816b26957dc9aba1a056e1252bfde8638e53f50337e9baef85f6c9d6467', 'aparajita@ssipmt.com', '9876543213'),
-	(6, '160138', 'pbkdf2:sha256:600000$vwh6BpPW1bvOGrw5$225af984d65ba7f396fd1b8dfc0ba499348dcdd95038c04898b0839552e6e06c', 'aparajita@ssipmt.com', 'vv');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
